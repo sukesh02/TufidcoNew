9475b75700757eea145361dcb4cca24b3b49d2f5
sukesh02 <sukeshraghav@gmail.com>
2024-03-27 11:58:13 +0530

    fix/routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FundsComponent } from './funds/funds.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'funds', component:FundsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
